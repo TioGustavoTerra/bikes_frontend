ff1c2ff72583c3b5ddf99ce9c91cf165b474d067
[NTT DATA]Reverson Batista Mendes <reverson.batistamendes@nttdata.com>
2023-06-14 15:19:04 -0300

    stylo link

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -188,6 +188,7 @@ class _LoginPageState extends State<LoginPage> {
                                               ),
                                               const SizedBox(width: 4),
                                               TextButton(
+                                                style: ButtonStyle( ..coloca aqui),
                                                 onPressed: () {
                                                   Navigator.pushNamed(
                                                       context, '/registration');
