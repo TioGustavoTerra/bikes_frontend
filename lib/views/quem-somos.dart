949a4d168a238cf282a0da792a01a361d9dc7fad
Gabriel <gabrielsantosterra@gmail.com>
2023-06-28 16:52:02 -0300

    imagens Págnia sobre

--- lib/views/quem-somos.dart
+++ lib/views/quem-somos.dart
@@ -16,7 +16,7 @@ class _QuemsomosState extends State<Quemsomos> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
+      backgroundColor: Colors.white,
       appBar: AppBar(
         toolbarHeight: 75,
         title: const Cabecalho(),
@@ -32,15 +32,6 @@ class _QuemsomosState extends State<Quemsomos> {
           child: Stack(
             alignment: Alignment.center,
             children: [
-              Image.network(
-                'assets/site-sistema/cadastro/fundo-cadastro.jpg',
-                width: MediaQuery.of(context).size.width * 40,
-                height: MediaQuery.of(context).size.height * 40,
-                fit: BoxFit.cover,
-              ),
-              Container(
-                color: const Color.fromRGBO(30, 122, 97, 100),
-              ),
 
               Padding(
                 padding: const EdgeInsets.only(bottom: 800),
@@ -48,13 +39,15 @@ class _QuemsomosState extends State<Quemsomos> {
                   children: [
                     Expanded(
                         child: Container(
+                          color: Colors.orange,
                           width: MediaQuery.of(context).size.width,
-                          color: Colors.blue,
-                          child: Image.asset('assets/site-sistema/Quem-somos/topo-quem-somos.jpg'),
-                        ))
-                  ],
-                ),
-              ),
+                          child: Image.network('assets/site-sistema/Quem-somos/topo-quem-somos.jpg')
+                        )),
+                          ],
+                        ),
+                      ),
+
+
               Positioned(
                 left: 0,
                 right: 0,
@@ -66,7 +59,7 @@ class _QuemsomosState extends State<Quemsomos> {
                       children: [
 
                         Padding(
-                            padding: const EdgeInsets.only(left: 370, top: 200, bottom: 300),
+                            padding: const EdgeInsets.only(left: 500, top: 200, bottom: 300),
 
                             child: Column(
                               children: [
@@ -76,7 +69,7 @@ class _QuemsomosState extends State<Quemsomos> {
                                      child: const Text('Quem somos\n\n'
                                        'A Bikes.com.br está sendo pensada para se tornar a referência nacional quando se tratar das atividades relacionadas ao ciclismo. Sejam elas, atividades de lazer, competições, organização de eventos, compra e venda de Bikes novas e usadas, acessórios, peças, nutrição e muito mais...\n\n'
                                           'Nos posicionamos como uma Startup ligada ao escossistema tecnológico fomentado no Sudoeste do Paraná, a empresa nasce com o objeito de entregar valor para todos os envolvidos no mundo das Bikes. Nasce do sonhos de seus CoFundadores, Gustavo Eduardo Terra e Eduardo Gaspareto, com o objetivo de criar um negócio sustentável, promissor, socialmente correto mas principalmente fomentar a prática de um esporte fantástico que trás muita possibilidade. Vem com a gente!!! Clique aqui para ser avisado do lançamento.\n\n'
-                                          'Deus abençoe!!!', style: TextStyle(color: Colors.white),),
+                                          'Deus abençoe!!!', style: TextStyle(color: Colors.black),),
                                     ))
                               ],
                             )
@@ -89,30 +82,32 @@ class _QuemsomosState extends State<Quemsomos> {
                           children: [
                             Expanded(
                                 child: Container(
-                                  width: MediaQuery.of(context).size.width / 5,
-                                  color: Colors.green,
-                                  child: Image.asset('assets/site-sistema/Quem-somos/para-esporte.jpg'),
-                            )),
+                                  width: MediaQuery.of(context).size.width / 7,
+                                  child: Column(
+                                    children: [
+                                      Image.network('assets/site-sistema/Quem-somos/para-esporte.jpg'),
+                                     // const Text('Olá mundo', textAlign: TextAlign.center,)
+                                      ]))),
+
+                            const SizedBox(height: 20),
 
                             Expanded(
                                 child: Container(
-                                  width: MediaQuery.of(context).size.width / 5,
-                                  color: Colors.yellow,
-                                  child: Image.asset('assets/site-sistema/Quem-somos/para-passeio.jpg'),
+                                  width: MediaQuery.of(context).size.width / 7,
+                                  child: Image.network('assets/site-sistema/Quem-somos/para-passeio.jpg'),
                                 ))
                           ],
                         ),
                         ),
                         Padding(
-                            padding: const EdgeInsets.only(left: 50, top: 200, bottom: 300),
+                            padding: const EdgeInsets.only(left: 20, top: 200, bottom: 300),
 
                         child: Column(
                           children: [
                             Expanded(
                                 child: Container(
-                                  width: MediaQuery.of(context).size.width / 5,
-                                  color: Colors.pink,
-                                  child: Image.asset('assets/site-sistema/Quem-somos/para-aventura.jpg'),
+                                  width: MediaQuery.of(context).size.width / 7,
+                                  child: Image.network('assets/site-sistema/Quem-somos/para-aventura.jpg', width: 100,),
                             ))
                           ],
                         )
