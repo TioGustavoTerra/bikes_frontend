7251520c5718dccc2ffb806dbf40fc23db640776
[NTT DATA]Reverson Batista Mendes <reverson.batistamendes@nttdata.com>
2023-07-28 16:29:08 -0300

    pesquisa tela home

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -38,36 +38,7 @@ class _HomePageState extends State<HomePage> {
             drawer: Responsive.isMobile(context)
                 ? const Drawer(child: DrawerApp())
                 : null,
-            body: Align(
-              alignment: Alignment.center,
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(maxWidth: 1400),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                          width: 600,
-                          height: 400,
-                          child:
-                              Column(children: [ColorOptions(), TextField()])),
-                      SizedBox(
-                          width: 80,
-                          height: 80,
-                          child: IconButton.filled(
-                              style: ButtonStyle(
-                                  minimumSize: MaterialStateProperty.all(
-                                      const Size(80, 80)),
-                                  shape: MaterialStateProperty.all(
-                                    const RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.zero,
-                                    ),
-                                  )),
-                              onPressed: () {},
-                              icon: const Icon(Icons.search)))
-                    ]),
-              ),
-            ));
+            body: const MarketplaceBikes());
       },
     );
   }
@@ -81,49 +52,5 @@ class _HomePageState extends State<HomePage> {
       });
     }
     //nomeUsuario = user['nome_usuario'];
-  }
-}
-
-enum Color { red, green }
-
-class ColorOptions extends StatefulWidget {
-  const ColorOptions({super.key});
-
-  @override
-  State<ColorOptions> createState() => _ColorOptionsState();
-}
-
-class _ColorOptionsState extends State<ColorOptions> {
-  Color _selectedColor = Color.red;
-
-  @override
-  Widget build(BuildContext context) {
-    return SegmentedButton<Color>(
-      style: ButtonStyle(
-          minimumSize: MaterialStateProperty.all(const Size(600, 400)),
-          shape: MaterialStateProperty.all(
-            const RoundedRectangleBorder(
-              borderRadius: BorderRadius.zero,
-            ),
-          )),
-      selected: <Color>{_selectedColor},
-      showSelectedIcon: false,
-      onSelectionChanged: (Set<Color> newSelection) {
-        setState(() {
-          _selectedColor = newSelection.first;
-        });
-      },
-      segments: const <ButtonSegment<Color>>[
-        ButtonSegment<Color>(
-          value: Color.red,
-          label: Text('Bikes'),
-        ),
-        ButtonSegment<Color>(
-          value: Color.green,
-          label: Text('Peças e Acessórios'),
-        ),
-      ],
-      // style: buttonStyle,
-    );
   }
 }
