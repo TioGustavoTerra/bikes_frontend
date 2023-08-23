7fa21a05ccb14d65fd7fb482909dd368d34a28d1
Gabriel <gabrielsantosterra@gmail.com>
2023-08-23 13:39:43 -0300

    Ignorar

--- lib/features/about/quem_somos.dart
+++ lib/features/about/quem_somos.dart
@@ -2,7 +2,9 @@ import 'package:bikes_frontend/componentes/DrawerApp.dart';
 // import 'package:bikes_frontend/componentes/sobre.dart';
 import 'package:bikes_frontend/componentes/cabecalho.dart';
 import 'package:bikes_frontend/componentes/cabecalhoapp.dart';
+import 'package:bikes_frontend/componentes/rodape.dart';
 import 'package:bikes_frontend/utils/responsive.dart';
+
 import 'package:flutter/material.dart';
 
 // import '../../componentes/rodape.dart';
@@ -13,8 +15,6 @@ import '../../componentes/rodapeApp.dart';
 import '../../componentes/sobre_img.dart';
 
 class QuemSomos extends StatelessWidget {
-  const QuemSomos({super.key});
-
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
@@ -33,7 +33,6 @@ class QuemSomos extends StatelessWidget {
           child: Column(
             children: [
               const TopsectionSobre(),
-<<<<<<< HEAD
                Row(children: [
                    if (Responsive.isMobile(context))
                   const PreferredSize(
@@ -56,56 +55,6 @@ class QuemSomos extends StatelessWidget {
                   preferredSize: const Size(double.infinity, 72),
                   child: Rodape(),
                 )),
-=======
-              RichText(
-                text: TextSpan(
-                  text: 'Quem somos \n'
-                      'A Bikes.com.br está sendo pensada para se tornar a referência\n'
-                      'nacional quando se tratar des atividades relacionadas ao ciclismo.\n'
-                      'Sejam elas, atividades de lazer, competições, organização de eventos,\n'
-                      'compra e vende de Bikes novas ou usadas, acessórios, peças, nutrição e muito mais...\n\n'
-                      'Nos posicionamos como uma Startup ligada ao ecossistema\n'
-                      'tecnológico fomentado no Sudoeste do Paraná, a empresa nasce\n'
-                      'com o objetivo de entregar valor para todos os envolvidos no mundo\n'
-                      'das Bikes. Nasce dos sonhos de seus CoFundadores, Gustavo\n'
-                      'Eduardo Terra e Eduardo Gaspareto, com o objetivo de criar um\n'
-                      'negócio sustentável, promissor, socialmente correto mas\n'
-                      'principalmente fomentar a prática de um esporte fantástco que trás\n'
-                      'muita possibilidade. Vem com a gente!!!',
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: ' Clique aqui para ser \navisado do lançamento\n\n',
-                      style: const TextStyle(
-                        color: Colors.black,
-                      ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Navigator.pushNamedAndRemoveUntil(
-                            context,
-                            "/home",
-                            ModalRoute.withName('/'),
-                          );
-                        },
-                    ),
-                    const TextSpan(text: 'Deus abençoe!!!'),
-                  ],
-                ),
-              ),
-              Column(
-                children: [
-                 SvgPicture.network(
-                   'site-sistema/Quem-somos/para_esporte.svg',
-                   fit: BoxFit.cover,
-                 ),
-                  SvgPicture.network(
-                    'site-sistema/Quem-somos/para_passeio.svg',
-                    fit: BoxFit.cover,
-                  ),
-                ],
-              ),
-              SvgPicture.network(
-                  'site-sistema/Quem-somos/para_passeio.svg'),
->>>>>>> ce2d96c8195985aba1b49b2f695e7841c57f57a3
             ],
           ),
         ),
