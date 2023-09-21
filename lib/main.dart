0cec400060aa0a245386cb68256ee3c7f60cd792
Gabriel <gabrielsantosterra@gmail.com>
2023-09-21 15:04:55 -0300

    Debug 2

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,7 @@ class MyApp extends StatelessWidget {
       title: 'bikes.com.br',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
-      // darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
-      initialRoute: '/home',
+            initialRoute: '/home',
       routes: {
         '/teste': (context) => Teste(),
         '/perfil': (context) => const PerfilScreen(),
@@ -34,6 +33,8 @@ class MyApp extends StatelessWidget {
         '/comprar': (context) => const Comprar(),
         '/contato': (context) => Contato(),
       },
+      // home: Vender(),
+      // home:Comprar()
     );
   }
-}
+} 
